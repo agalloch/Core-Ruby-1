42ca0e82d55b30e6e21e669e7c914c2088809497
agalloch <the.dreadful.hours@gmail.com>
2014-10-27 23:04:38 +0200

    hash tasks' solutions

--- week1/2-hashes/solution.rb
+++ week1/2-hashes/solution.rb
@@ -1,3 +1,18 @@
 class Hash
-  # Your code goes here.
+  def pick(*filter_set)
+    select { |key, _| filter_set.include? key }
+  end
+
+  def except(*filter_set)
+    reject { |key, _| filter_set.include? key }
+  end
+
+  def compact_values
+    select { |_, value| true if value }
+  end
+
+  def defaults(other_hash)
+    diff = other_hash.except(*keys)
+    (result = Hash[self]).tap { diff.each { |k, v| result[k] = v } }
+  end
 end
--- week1/2-hashes/solution_test.rb
+++ week1/2-hashes/solution_test.rb
@@ -6,4 +6,24 @@ class HashTest < Minitest::Test
   def test_the_truth
     assert true
   end
+
+  def test_pick
+    expected_hash = { a: 1, b: 2 }
+    assert_equal expected_hash, { a: 1, b: 2, c: 3 }.pick(:a, :b)
+  end
+
+  def test_except
+    expected_hash = { a: 1, b: 2 }
+    assert_equal expected_hash, { a: 1, b: 2, d: nil }.except(:d)
+  end
+
+  def test_compact_values
+    expected_hash = { a: 1, b: 2 }
+    assert_equal expected_hash, { a: 1, b: 2, c: false, d: nil }.compact_values
+  end
+
+  def test_defaults
+    expected_hash = { a: 1, b: 2, c: 3 }
+    assert_equal expected_hash, { a: 1, b: 2 }.defaults(a: 4, c: 3)
+  end
 end
