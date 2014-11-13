e2bf39d8c8f3650aa0fc29f7a1821960d4dc6167
agalloch <the.dreadful.hours@gmail.com>
2014-11-14 01:39:32 +0200

    Enumerable finished

--- week1/3-my-enumerable/solution.rb
+++ week1/3-my-enumerable/solution.rb
@@ -1,20 +1,22 @@
+require 'pry'
+
 module MyEnumerable
   def map
     return each unless block_given?
 
-    (res = []).tap { each { |el| res << yield(el) } }
+    [].tap { |res| each { |el| res << yield(el) } }
   end
 
   def filter
     return each unless block_given?
 
-    (res = []).tap { each { |el| res << el if yield el } }
+    [].tap { |res| each { |el| res << el if yield el } }
   end
 
   def reject
     return each unless block_given?
 
-    (res = []).tap { each { |el| res << el unless yield el } }
+    [].tap { |res| each { |el| res << el unless yield el } }
   end
 
   def reduce(initial = nil, symbol = nil)
@@ -46,11 +48,28 @@ module MyEnumerable
     initial
   end
 
+  alias_method :collect, :map
+  alias_method :select, :filter
+  alias_method :fold, :reduce
+
   def any?(&block)
     block ||= proc { |obj| obj }
     each { |el| return true if block.call el }
 
     false
+  end
+
+  def one?
+    memo = false
+    each do |el|
+      p = block_given? ? yield(el) : el
+
+      return false if p && memo
+
+      memo = true if p
+    end
+
+    memo
   end
 
   def all?(&block)
@@ -61,7 +80,17 @@ module MyEnumerable
   end
 
   def each_cons(n)
-    # Your code goes here.
+    return nil if n > size || n <= 0
+    return each unless block_given?
+
+    sub = []
+    each do |el|
+      sub << el
+      sub.shift if sub.size > n
+      yield sub.dup if sub.size == n
+    end
+
+    nil
   end
 
   def include?(element)
@@ -87,5 +116,119 @@ module MyEnumerable
 
   def size
     each.size
+  end
+
+  def group_by
+    return each unless block_given?
+
+    groups = {}
+
+    each do |el|
+      factor = yield(el)
+
+      if groups[factor]
+        groups[factor] << el
+      else
+        groups[factor] = [el]
+      end
+    end
+
+    groups
+  end
+
+  def min(&block)
+    comp = block_given? ? block : -> x, y { x <=> y }
+
+    res = each.peek
+    each do |el|
+      res = el if comp.call(el, res) < 0
+    end
+
+    res
+  end
+
+  def min_by(&block)
+    return each unless block_given?
+
+    min(&block)
+  end
+
+  def max(&block)
+    comp = block_given? ? block : -> x, y { x <=> y }
+
+    res = each.peek
+    each do |el|
+      res = el if comp.call(el, res) > 0
+    end
+
+    res
+  end
+
+  def max_by(&block)
+    return each unless block_given?
+
+    max(&block)
+  end
+
+  def minmax(&block)
+    comp = block_given? ? block : -> x, y { x <=> y }
+
+    min = max = each.peek
+    each do |el|
+      min = el if comp.call(el, min) < 0
+      max = el if comp.call(el, max) > 0
+    end
+
+    [min, max]
+  end
+
+  def minmax_by(&block)
+    return each unless block_given?
+
+    minmax(&block)
+  end
+
+  def take(n)
+    fail ArgumentError, "Cannot take #{n} items from collection" if n.to_i < 0
+
+    [].tap do |res|
+      each do |el|
+        break if res.size == n.to_i
+        res << el
+      end
+    end
+  end
+
+  def take_while
+    return each unless block_given?
+
+    [].tap do |res|
+      each do |el|
+        break unless yield el
+        res << el
+      end
+    end
+  end
+
+  def drop(n)
+    fail ArgumentError, "Cannot drop #{n} items from collection" if n.to_i < 0
+
+    index = 0
+    [].tap do |res|
+      each do |el|
+        res << el if index >= n
+        index += 1
+      end
+    end
+  end
+
+  def drop_while
+    return each unless block_given?
+
+    [].tap do |res|
+      each do |el|
+        res << el unless yield el
+      end
+    end
   end
 end
--- week1/3-my-enumerable/solution_test.rb
+++ week1/3-my-enumerable/solution_test.rb
@@ -25,11 +25,19 @@ class SolutionTest < Minitest::Test
   end
 
   def test_map
-    assert_equal [2, 3, 4, 5, 6], Collection.new(*1..5).map(&:succ)
+    assert_equal((2..6).to_a, Collection.new(*1..5).map(&:succ))
+  end
+
+  def test_collect
+    assert_equal((2..6).to_a, Collection.new(*1..5).collect(&:succ))
   end
 
   def test_filter
     assert_equal [1, 3, 5, 7, 9], @collection.filter(&:odd?)
+  end
+
+  def test_select
+    assert_equal [1, 3, 5, 7, 9], @collection.select(&:odd?)
   end
 
   def test_reject
@@ -41,11 +49,11 @@ class SolutionTest < Minitest::Test
   end
 
   def test_reduce_initial_and_symbol
-    assert_equal (-3628800), @collection.reduce(-1, :*)
+    assert_equal(-3_628_800, @collection.reduce(-1, :*))
   end
 
   def test_reduce_symbol
-    assert_equal (-53), @collection.reduce(:-)
+    assert_equal(-53, @collection.reduce(:-))
   end
 
   def test_reduce_block
@@ -53,11 +61,27 @@ class SolutionTest < Minitest::Test
   end
 
   def test_reduce_symbol_and_block
-    assert_equal 100, @collection.reduce(:+) { |memo, item| item**2 }
+    assert_equal 100, @collection.reduce(:+) { |_, item| item**2 }
   end
 
-  def test_reduce_no_params
-    assert_equal 4, @collectino.reduce
+  def test_fold_initial_and_block
+    assert_equal 55, @collection.fold(0) { |memo, item| memo + item }
+  end
+
+  def test_fold_initial_and_symbol
+    assert_equal(-3_628_800, @collection.fold(-1, :*))
+  end
+
+  def test_fold_symbol
+    assert_equal(-53, @collection.fold(:-))
+  end
+
+  def test_fold_block
+    assert_equal 385, @collection.fold { |memo, item| memo + item**2 }
+  end
+
+  def test_fold_symbol_and_block
+    assert_equal 100, @collection.fold(:+) { |_, item| item**2 }
   end
 
   def test_include?
@@ -71,6 +95,28 @@ class SolutionTest < Minitest::Test
 
   def test_all?
     assert_equal true, @collection.all?
+  end
+
+  def test_one?
+    assert_equal true, Collection.new(nil, nil, 5, false, false).one?
+  end
+
+  def test_one_negative
+    assert_equal false, Collection.new(1, 2, 3, 4, nil).one?
+  end
+
+  def test_one_with_block
+    @collection = Collection.new(
+      'uno', 'four', 'equatorial skunk', 'frog-stomp')
+
+    assert_equal true, @collection.one? { |item| item.include? '-' }
+  end
+
+  def test_one_with_block_negative
+    @collection = Collection.new(
+      'Highlander', 'Neo', 'Mr. Anderson', 'Mr. Bean', 'Neo')
+
+    assert_equal false, @collection.one? { |item| item == 'Neo' }
   end
 
   def test_size
@@ -88,5 +134,75 @@ class SolutionTest < Minitest::Test
 
   def test_count_block
     assert_equal 7, @collection.count { |x| x.even? || x % 3 == 0 }
+  end
+
+  def test_each_cons
+    n = 6
+    actual = []
+    size = @collection.size
+    @collection.each_cons(n) { |sub| actual << sub.include?(n) }
+
+    assert_equal [true] * (size - n + 1), actual
+  end
+
+  def test_group_by
+    assert_equal({ 1 => [1, 8], 2 => [2, 9], 3 => [3, 10], 4 => [4], 5 => [5],
+                   6 => [6], 0 => [7] }, @collection.group_by { |el| el % 7 })
+  end
+
+  def test_min
+    assert_equal 1, @collection.min
+  end
+
+  def test_min_with_block
+    actual = Collection.new('Peter Pan', 'Donald Duck', 'Ron Jeremy',
+                            'Snowwhite')
+    assert_equal 'Ron Jeremy',
+                 actual.min { |x, y| (x.length % 5) <=> (y.length % 5) }
+  end
+
+  def test_min_by
+    actual = Collection.new('Peter Pan', 'Donald Duck', 'Ron Jeremy',
+                            'Snowwhite')
+    assert_equal 'Ron Jeremy',
+                 actual.min_by { |x, y| (x.length % 5) <=> (y.length % 5) }
+  end
+
+  def test_max
+    assert_equal 10, @collection.max
+  end
+
+  def test_max_with_block
+    actual = Collection.new('Peter Pan', 'Donald Duck', 'Ron Jeremy',
+                            'Snowwhite')
+    assert_equal 'Peter Pan',
+                 actual.max { |x, y| (x.length % 5) <=> (y.length % 5) }
+  end
+
+  def test_max_by
+    actual = Collection.new('Peter Pan', 'Donald Duck', 'Ron Jeremy',
+                            'Snowwhite')
+    assert_equal 'Peter Pan',
+                 actual.max_by { |x, y| (x.length % 5) <=> (y.length % 5) }
+  end
+
+  def test_minmax
+    assert_equal [1, 10], @collection.minmax
+  end
+
+  def test_take
+    assert_equal [1, 2, 3, 4], @collection.take(4)
+  end
+
+  def test_take_while
+    assert_equal((1..5).to_a, @collection.take_while { |e| e < 6 })
+  end
+
+  def test_drop
+    assert_equal((7..10).to_a, @collection.drop(6))
+  end
+
+  def test_drop_while
+    assert_equal((7..10).to_a, @collection.drop_while { |e| e < 7 })
   end
 end
